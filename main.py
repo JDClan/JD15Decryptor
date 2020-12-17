44545e7931c79b556668495791a322982ba8ca71
mishok-jd <68820077+mishok-jd@users.noreply.github.com>
2020-12-17 20:30:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -269,7 +269,10 @@ while i < markerscount:
     markers.append(marker)
     print("MusicTrack markers ready: " + str(i) + "/" + str(markerscount - 1))
     i += 1
-finaljson = dict(markers = markers, startbeat = 0, endbeat = markerscount)
+a += 475
+b += 475
+videostarttime = struct.unpack("!f", bytes.fromhex(musictrack[a:b]))
+finaljson = dict(markers = markers, startbeat = 0, endbeat = markerscount, videostarttime = videostarttime)
 with open('output/' + codename + '_musictrack.json', "w", encoding='utf-8') as f:
       json.dump(finaljson, f, ensure_ascii=False)
 print("MusicTrack data JSON is ready!")
